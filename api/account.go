c2c713f2f283804c407917f7e82a3d35c3e8fb96
Akinakk <Artemi.Baburin@tptlive.ee>
2024-08-01 18:49:27 +0300

    feat: mock db for get account test, 100% coverage

--- api/account.go
+++ api/account.go
@@ -41,24 +41,24 @@ type getAccountRequest struct {
 }
 
 func (server *Server) getAccount(ctx *gin.Context) {
-	var req getAccountRequest
-	if err := ctx.ShouldBindUri(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+    var req getAccountRequest
+    if err := ctx.ShouldBindUri(&req); err != nil {
+        ctx.JSON(http.StatusBadRequest, errorResponse(err))
+        return
+    }
 
-	account, err := server.store.GetAccount(ctx, req.ID)
-	if err != nil {
-		if err == sql.ErrNoRows{
-			ctx.JSON(http.StatusNotFound, errorResponse(err))
-			return
-		}
+    account, err := server.store.GetAccount(ctx, req.ID)
+    if err != nil {
+        if err == sql.ErrNoRows {
+            ctx.JSON(http.StatusNotFound, errorResponse(err))
+            return
+        }
 
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
+        ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+        return
+    }
 
-	ctx.JSON(http.StatusOK, account)
+    ctx.JSON(http.StatusOK, account)
 }
 
 type listAccountRequest struct {
